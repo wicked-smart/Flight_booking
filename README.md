cfea16b2111bcd5d5a38a7fdf0180e1abfab9cec
wicked-smart <marzaanvan@gmail.com>
2025-06-10 12:12:07 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Flight booking web-app
+
+
---
+++ flight_booking/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FlightBookingConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'flight_booking'
